9288dac36863792c6825a24b90d654ca9b22f7c4
Alexandre Fraschini <alexandre.fraschini@gmail.com>
2020-05-15 19:53:14 +0200

    generated models with validations

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true },
+                     inclusion: { in: (0..5),
+                                  message: "Rating must be a number between 0 and 5"}
+end
