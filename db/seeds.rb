ae6d8829567fed58da1b5eb84d4feb02baa79abe
Alexandre Fraschini <alexandre.fraschini@gmail.com>
2020-05-15 20:10:32 +0200

    seed restaurants with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating 5 restaurants..."
+5.times do |i|
+  restaurant = Restaurant.create!(
+    name: Faker::Games::Zelda.unique.game,
+    address: Faker::Address.unique.full_address,
+    phone_number: Faker::PhoneNumber.unique.phone_number_with_country_code,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  puts "#{i + 1} - [#{restaurant.category}] #{restaurant.name} (#{restaurant.address})"
+end
+
+puts "Finished!"
